a258c30a4e522476b8304e71f08a1aacbddc78f5
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2025-03-17 22:14:39 +0300

    v0.1.05

--- js/mod.js
+++ js/mod.js
@@ -12,15 +12,19 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1.04",
+	num: "0.1.05",
 	name: "The Ranking",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.1.0.4</h3><br>
+	<h3>v0.1.05</h3><br>
+		- Some more additions to the style.
+		- Several bugfixes with implemented new layer features causing bugs, because new layer basically doesn't exist in update :|
+		- Changed layer names to symbols.
+	<h3>v0.1.04</h3><br>
 		- Fixed several automation bugs.
 		- Changed layer components style.
-	<h3>v0.1.0.3</h3><br>
+	<h3>v0.1.03</h3><br>
 		- Reduced the cost of [ Ranking XVII ] upgrade (3e269 -> 3e261)
 		- Reduced the 2nd Rankings effect softcap increase while in [ Rankings UnLimited ] challenge (0.325 -> 0.225)
 		- Also fixed 2nd Ranking's autobuyer not working while in challenges.
